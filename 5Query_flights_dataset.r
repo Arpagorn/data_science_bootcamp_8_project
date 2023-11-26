38a6fa6b552503416afa8fa50814f0399908b818
Arpagorn <141759982+Arpagorn@users.noreply.github.com>
2023-11-26 12:27:30 +0700

    Update 5Query_flights_dataset.r

--- 5Query_flights_dataset.r
+++ 5Query_flights_dataset.r
@@ -1,6 +1,4 @@
 ##HW01 - 5 Questions ask about flights data set
-##modifies add comments
-##add new comments test conncet with gits
 
 view(airlines)
 ## Q1: Which five airlines traveled the most in the year 2013?
